edde3ad020ede88833c66b91f2fdae3234ec2513
inbo <inbo2002@naver.com>
2026-07-18 15:13:57 +0900

    260718 엣지 케이스 유념하기. 공집합, dp인데 하나뿐인 상태.

---
+++ 128.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums.size() == 0)
+            return 0;
+        vector<int> dp(nums.size(), 1);
+        int res=1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] - nums[i-1] == 1)
+                dp[i] = dp[i-1] +1;
+            else if (nums[i] == nums[i-1])
+                dp[i] = dp[i-1];
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
