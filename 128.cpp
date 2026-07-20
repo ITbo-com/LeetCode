8386295f2573a3fa7c4a17f59e008638948fb458
inbo <inbo2002@naver.com>
2026-07-20 20:02:21 +0900

    260720 dp 중복 가능 냅색. 이전 상태에서 가져오는 값이 무엇인지 명확하게 알기

--- 128.cpp
+++ 128.cpp
@@ -4,19 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if(nums.size() == 0)
-            return 0;
-        vector<int> dp(nums.size(), 1);
-        int res=1;
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] - nums[i-1] == 1)
-                dp[i] = dp[i-1] +1;
-            else if (nums[i] == nums[i-1])
-                dp[i] = dp[i-1];
-            res = max(res, dp[i]);
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned int> dp(amount + 1, 0);
+
+        dp[0] = 1;
+        for(int j=0; j<coins.size(); j++){
+            vector<bool> used(amount+1, false);
+            used[0] = true;
+            for(int i=0; i<= amount; i++){
+                
+                if(i-coins[j] >= 0 && dp[i-coins[j]] > 0){
+                    dp[i] += dp[i-coins[j]];
+                    
+                    used[i] = true;
+                }
+            }
         }
-        return res;
+
+        return dp[amount];
     }
 };
