599382122988704dd07d08da92d744ca12985e78
inbo <inbo2002@naver.com>
2026-07-22 23:36:07 +0900

    260722. dp문제. 합/차 2개로 나뉠 때 어떻게 타겟으로 가는가를 수학적 원리로 냅색.. 거기에 0에 대한 처리와 음수 처리에 대한 고찰

--- 494.cpp
+++ 494.cpp
@@ -7,42 +7,25 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         
         int sum =0;
-        for(auto cur  : nums){
+        for(auto cur  : nums)
             sum += cur;
-        }
-        if(target > sum)
+
+        if((sum+target)%2 == 1 || abs(target) > sum)
             return 0;
-        vector<int> dp(sum*2 + 1, 0);
-        
-        for(int i=0; i<nums.size(); i++){
-            vector<int> dp1(sum*2 + 1, 0);
-            vector<int> dp2(sum*2 + 1, 0);
 
-            dp[sum+nums[i]]+=1;
-            dp[sum-nums[i]]+=1;
+        int temp = (sum+target)/2;
 
-            for(int j=0; j<=sum*2; j++){
-                if(j+nums[i] <= sum*2 && dp[j+nums[i]] > 0){
-                    if(j==sum)
-                        continue;
-                    dp1[j] = dp[j+nums[i]];
-                }
+        vector<int> dp(temp+1);
+        dp[0]=1;
+        for(int j=0; j<nums.size(); j++){
+            if(nums[j] == 0){
+                dp[0] *=2;
             }
-
-            for(int j=sum*2; j>=0; j--){
-                if (j-nums[i] >= 0 && dp[j-nums[i]] > 0){
-                    if(j==sum)
-                        continue;
-                    dp2[j] = dp[j-nums[i]];
-                }
+            for(int i=temp; i>=1; i--){
+                if(i-nums[j] >= 0 && dp[i-nums[j]] > 0)
+                    dp[i] += dp[i-nums[j]];
             }
-            for(int j=0; j<sum*2+1; j++){
-                dp[j] = dp1[j] + dp2[j];
-            }
-
         }
-    return dp[target + sum] + 1;
-
-
+        return dp[temp];
     }
 };
