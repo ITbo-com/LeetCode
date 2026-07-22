175a28560e2145d29de1b467c31e355aa5464319
inbo <inbo2002@naver.com>
2026-07-22 23:52:48 +0900

    260713 재도전 문제. LIS문제. O(N^2)풀이. 이분탐색을 통한 O(NlogN) 풀이로 다시 해보기

--- 300.cpp
+++ 300.cpp
@@ -1,3 +1,12 @@
+// [재도전] 260722
+// v1[i]의 의미를 "i번째를 마지막 원소로 쓰는 LIS의 길이"로 정의하기까지 오래 걸림.
+// 이 정의만 잡히면 이중 루프는 따라오는데, 정의를 스스로 세우는 게 어려웠음.
+// O(n log n) 이분탐색(lower_bound) 풀이는 따로 연습할 것.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
