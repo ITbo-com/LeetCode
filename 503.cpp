0167956235ce7bea31706e0f550252ec00cdedff
inbo <inbo2002@naver.com>
2026-07-30 18:48:43 +0900

    260730. 모노토닉 스택 방식으로 while 문 내부 아예 못 풀었음. 다시 풀기.

--- 503.cpp
+++ 503.cpp
@@ -6,22 +6,25 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int tot = nums.size();
-        vector<int> ans(tot, -1);
-
+        vector<int> pr(tot*2);
         for(int i=0; i<tot; i++){
-            int j = i+1;
-            while(true){
-                if (j==tot)
-                    j=0;
-                if(j==i)
-                    break;
-                if(nums[j] > nums[i]){
-                    ans[i]= nums[j];
-                    break;
+            pr[i] = nums[i];
+            pr[i+tot] = nums[i];
+        }
+
+        vector<int> ans(tot, -1);
+        stack<int> s1;
+
+        for(int i=0; i<tot * 2; i++){
+            
+            while(!s1.empty() && pr[i] > pr[s1.top()]){
+                if(ans[s1.top()%tot] == -1){
+                    ans[s1.top() % tot] = pr[i];
                 }
-                
-                j++;
+                s1.pop();
             }
+
+            s1.push(i);
 
         }
 
